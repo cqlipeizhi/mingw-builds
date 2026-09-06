5f9b0f7acc063ae25632b1ac62ecb86c23096520
lpz <cqlipeizhi@126.com>
2026-09-06 18:54:04 +0800

    修改host mingw version

--- library/config-win.sh
+++ library/config-win.sh
@@ -35,8 +35,8 @@
 
 # **************************************************************************
 
-readonly HOST_MINGW_VERSION=4.8.5
-readonly HOST_MINGW_RT_VERSION=4
+readonly HOST_MINGW_VERSION=4.8.4
+readonly HOST_MINGW_RT_VERSION=3
 readonly HOST_MINGW_BUILD_REV=0
 readonly i686_HOST_MINGW_PATH_URL="https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/$HOST_MINGW_VERSION/threads-posix/{exceptions}/i686-$HOST_MINGW_VERSION-release-posix-{exceptions}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
 readonly x86_64_HOST_MINGW_PATH_URL="https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/$HOST_MINGW_VERSION/threads-posix/{exceptions}/x86_64-$HOST_MINGW_VERSION-release-posix-{exceptions}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
