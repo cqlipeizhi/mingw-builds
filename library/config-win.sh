860e26d8bfe4d8922a455efd5500bc239fd0e119
lpz <cqlipeizhi@126.com>
2026-09-09 15:09:51 +0800

    HOST_MINGW_VERSION 4.9.4

--- library/config-win.sh
+++ library/config-win.sh
@@ -35,11 +35,12 @@
 
 # **************************************************************************
 
-readonly HOST_MINGW_VERSION=5.4.0
-readonly HOST_MINGW_RT_VERSION=5
+readonly HOST_MINGW_VERSION=4.9.4
+readonly HOST_MINGW_RT_VERSION=6
 readonly HOST_MINGW_BUILD_REV=0
-readonly i686_HOST_MINGW_PATH_URL="https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/$HOST_MINGW_VERSION/threads-posix/{exceptions}/i686-$HOST_MINGW_VERSION-release-posix-{exceptions}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
-readonly x86_64_HOST_MINGW_PATH_URL="https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/$HOST_MINGW_VERSION/threads-posix/{exceptions}/x86_64-$HOST_MINGW_VERSION-release-posix-{exceptions}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
+readonly i686_HOST_MINGW_PATH_URL="https://github.com/cqlipeizhi/mingw-builds-binaries/releases/download/$HOST_MINGW_VERSION-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV/i686-$HOST_MINGW_VERSION-release-posix-{exceptions}-{msvcrt}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
+readonly x86_64_HOST_MINGW_PATH_URL="https://github.com/cqlipeizhi/mingw-builds-binaries/releases/download/$HOST_MINGW_VERSION-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV/x86_64-$HOST_MINGW_VERSION-release-posix-{exceptions}-{msvcrt}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
+
 
 # **************************************************************************
 
