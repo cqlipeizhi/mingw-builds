525bb2fcc8066d15c93c9cc7bfed38cd180ad702
lpz <cqlipeizhi@126.com>
2026-09-09 11:57:51 +0800

    gcc 5.5.0

--- scripts/binutils.sh
+++ scripts/binutils.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.27
+PKG_VERSION=2.29.1
 PKG_NAME=binutils-${PKG_VERSION}
 [[ $USE_MULTILIB == yes ]] && {
 	PKG_NAME=$BUILD_ARCHITECTURE-$PKG_NAME-multi
@@ -53,21 +53,12 @@ PKG_PRIORITY=prereq
 #
 
 PKG_PATCHES=(
-	$( [[ $PKG_VERSION == 2.27 ]] && { 
-			echo "binutils/0001-enable-gold-on.mingw32.patch"
-			echo "binutils/binutils-2.27-0002-check-for-unusual-file-harder.patch"
-		} || {
-			echo "binutils/0002-check-for-unusual-file-harder.patch"
-		}
-	)
+	binutils/0001-enable-gold-on.mingw32.patch
+	binutils/0002-check-for-unusual-file-harder.patch
 	binutils/0008-fix-libiberty-makefile.mingw.patch
 	binutils/0009-fix-libiberty-configure.mingw.patch
-	$( [[ $PKG_VERSION == 2.27 ]] && {
-		echo "binutils/0110-binutils-mingw-gnu-print.patch"
-		echo "binutils/0018-binutils-PC64-PCRQUAD-relocation-fix.mingw32.patch"
-	} || {
-		echo "binutils/0022-libiberty-missing-typedef.patch"
-	} )
+	binutils/0110-binutils-mingw-gnu-print.patch
+
 )
 
 #
