7baa32415fea7c14c26b49eea08e69305c0cb3db
lpz <cqlipeizhi@126.com>
2026-09-12 07:49:34 +0800

    gcc 7.5.0

--- library/config.sh
+++ library/config.sh
@@ -94,7 +94,7 @@ LINK_TYPE_SHARED="--enable-shared --disable-static"
 LINK_TYPE_STATIC="--enable-static --disable-shared"
 LINK_TYPE_GCC_SHARED="--enable-host-shared"
 LINK_TYPE_GCC_STATIC="--disable-host-shared"
-LINK_TYPE_GCC=$LINK_TYPE_GCC_SHARED
+LINK_TYPE_GCC=$LINK_TYPE_BOTH
 GCC_DEPS_LINK_TYPE=$LINK_TYPE_STATIC
 
 ENABLE_LANGUAGES='c,c++,fortran'
