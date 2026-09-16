c8d3f23ed5329c07ddd338ea9003b1114413a7dc
lpz <cqlipeizhi@126.com>
2026-09-16 18:05:24 +0800

    sqlite 3.51.3

--- scripts/sqlite.sh
+++ scripts/sqlite.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=3510200
+PKG_VERSION=3510300
 PKG_NAME=sqlite-${PKG_VERSION}
 PKG_DIR_NAME=sqlite-autoconf-${PKG_VERSION}
 PKG_TYPE=.tar.gz
